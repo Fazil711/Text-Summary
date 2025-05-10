50091efc13149fb9bc8ffe2aa090335991468c34
Fazil711 <fazilahmedshaikh@gmail.com>
2025-05-11 00:39:24 +0530

    Changed port

--- main.py
+++ main.py
@@ -143,4 +143,4 @@ def rephrase():
     return jsonify({'rephrased': rephrased_text})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
