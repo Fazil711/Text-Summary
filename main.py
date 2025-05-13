9f637f7a082fc841c9eee02f260c4d6ba7534d23
Ameya310101 <ameyanile789@gmail.com>
2025-05-13 18:11:13 +0000

    Update main.py
    app.run old comented and new added

--- main.py
+++ main.py
@@ -142,4 +142,5 @@ def rephrase():
     return jsonify({'rephrased': rephrased_text})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    #app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=5000, debug=True)
