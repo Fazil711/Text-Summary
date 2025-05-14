2994516a8c29780f21e27ca088e6d922d4faeba9
Ameya310101 <ameyanile789@gmail.com>
2025-05-14 11:33:24 +0530

    Update main.py
    updated key back to normal

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 
-api_key = ("AIzaSyCdG20FhMAFU52KE7jDjMyo78koggfwMY4")
+api_key = ("GEN_API_KEY")
 if not api_key:
     raise ValueError("The GEMINI_API_KEY environment variable is not set.")
 
